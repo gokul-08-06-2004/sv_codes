bb51eed122e2047e5679868ab943f7491e264f1c
Gokul S <sarangokul86@gmail.com>
2025-11-10 23:13:27 +0530

    Update 01.one_dimensional_array.sv

--- Day_02/01.one_dimensional_array.sv
+++ Day_02/01.one_dimensional_array.sv
@@ -11,4 +11,29 @@ module one_dimensional_array;
       $display("arr[%0d]=%0d",i,arr[i]);
     end
   end
+
     endmodule
+
+
+
+module one_dimenional_array;
+  integer i;
+  logic [15:0]arr[3:0];
+  initial begin
+    arr[0]=16'haaaa;
+    $display("arr[0]=%h",arr[0]);
+    arr[1]=16'haaaa;
+    $display("arr[1]=%h",arr[1]);
+    arr[2]=16'hbbbb;
+    $display("arr[2]=%h",arr[2]);
+    arr[3]=16'hdddd;
+    $display("arr[3]=%h",arr[3]);
+    
+//     arr=16'hadcd;
+//     $display("arr=%h",arr);
+    
+//     for(i=0;i<4;i++)begin
+//       $display("arr[%0d]=%0d",i,arr[i]);
+//     end
+  end
+endmodule
