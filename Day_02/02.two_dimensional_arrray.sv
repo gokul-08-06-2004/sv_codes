b55e0921b6fa2b1abaf90b02a4c6496522ea7d65
Gokul S <sarangokul86@gmail.com>
2025-11-10 23:06:29 +0530

    Create 02.two_dimensional_arrray.sv

---
+++ Day_02/02.two_dimensional_arrray.sv
@@ -0,0 +1,12 @@
+// Code your design here
+module two_dimensional_arry;
+  integer i,j;
+  int arr[2][3]='{'{1,2,3},'{1,2,3}};
+  initial begin
+    for(i=0;i<2;i++)begin
+      for(j=0;j<3;j++)begin
+        $display("arr[%0d][%0d]=%0d",i,j,arr[i][j]);
+      end
+    end
+  end
+endmodule
