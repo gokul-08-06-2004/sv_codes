58bbe86ba16f29b928d4295fa2d1cbe7f6c3f154
Gokul S <sarangokul86@gmail.com>
2025-11-13 21:39:36 +0530

    Update 03.packed_structure.sv

--- Day_05/03.packed_structure.sv
+++ Day_05/03.packed_structure.sv
@@ -17,21 +17,3 @@ module packed_structure;
   end
 endmodule
 
-module unpacked_structure;
-  typedef struct {
-    int addr='haaffaaff;
-  bit [7:0]src[4]='{1,2,3,4};
-  logic [7:0]srt='h12;
-  }num;
-  num n1,n2;
-  
-  initial begin
-    $display("\n addr=%h \n src=%p \n srt=%0d",n1.addr,n1.src,n1.srt);
-    
-    n2.addr='haaaa_aaaa;
-    n2.src='{4,3,2,1};
-    n2.srt='h23;
-    
-    $display("\n addr=%h \n src=%p \n srt=%0d",n2.addr,n2.src,n2.srt);
-  end
-endmodule
