96b803ef16d5bdd172165d86ad38885ce8e383ea
Gokul S <sarangokul86@gmail.com>
2025-12-21 21:44:47 +0530

    Add non-blocking event example in SystemVerilog

---
+++ Day_14/05.non_blocking_event.sv
@@ -0,0 +1,27 @@
+// Code your testbench here
+// or browse Examples
+module m1;
+  event e1;
+  initial begin
+    $display("time=%0t event e1 triggered",$time);
+    ->>e1;
+  end
+  
+  initial begin
+    $display("time=%0t event e1 wait for the event and triggering for the event",$time);
+    wait(e1.triggered);
+    $display("time=%0t event e1 is done",$time);
+  end
+  
+  initial begin
+    $display("time=%0t event e1 @ for the event and triggering for the event",$time);
+    @(e1.triggered);
+    $display("time=%0t event e1 is done",$time);
+  end
+endmodule
+
+// # time=0 event e1 triggered
+// # time=0 event e1 wait for the event and triggering for the event
+// # time=0 event e1 @ for the event and triggering for the event
+// # time=0 event e1 is done
+// # time=0 event e1 is done
