62fa92efb8d3776b0297cba9bcc6c1b63e609373
Gokul S <sarangokul86@gmail.com>
2026-01-03 21:54:12 +0530

    Add if-else operator constraints in if_else.sv
    Implemented if-else constraints for random variables in SystemVerilog.

--- Day_16/05.if_else.sv
+++ Day_16/05.if_else.sv
@@ -30,3 +30,47 @@ endmodule
 // # num=1 data=2 addr=8
 // # num=0 data=2 addr=0
 // # num=0 data=5 addr=4
+
+
+                   
+// if else operator:
+class if_else_operator;
+  randc bit data;
+  rand bit[7:0]addr;
+  rand bit[7:0]src;
+  
+  constraint range{if(data==1)
+  {
+    addr<9;
+    src<6;
+  }
+    else{
+    addr<9;
+      src<8;}}
+endclass
+
+module m1;
+  if_else_operator i1;
+  initial begin
+    i1=new();
+    for(int i=0;i<6;i++)begin
+    if(i1.randomize())
+      $display("data=%0d addr=%0d src=%0d",i1.data,i1.addr,i1.src);
+    else
+      $display("randomization is failed");
+    end
+  end
+endmodule
+                   
+// // # data=1 addr=4 src=1
+// // # data=0 addr=7 src=7
+// // # data=0 addr=5 src=3
+// // # data=1 addr=4 src=3
+// // # data=1 addr=0 src=2
+// // # data=0 addr=5 src=7                   
+                   
+// //  Multiple randc variables… only the first will behave cyclic
+// // Meaning:
+// // data → true cyclic (0 → 1 → 0 → 1…
+// // addr & src → WILL NOT behave cyclic, they act like normal rand
+// // This is expected SystemVerilog behavior.
