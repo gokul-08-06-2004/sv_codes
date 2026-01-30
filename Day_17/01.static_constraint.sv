4bd1b6fa78f600bd7166e33dd5ff4ccd35cc871a
Gokul S <sarangokul86@gmail.com>
2026-01-30 10:01:42 +0530

    Update 01.static_constraint.sv

--- Day_17/01.static_constraint.sv
+++ Day_17/01.static_constraint.sv
@@ -125,3 +125,38 @@ module m1;
 // # data=0 addr=25
   end
 endmodule
+
+
+    class class1;  
+  rand bit [2:0] value;  
+  constraint cons { value == 1; }  
+endclass  
+
+class class2;  
+  rand bit [2:0] value;  
+  static constraint cons { value == 1; }  
+endclass  
+
+initial begin  
+  object_1.cons.constraint_mode(0);  
+
+  for (int i = 1; i <= 3; i++) begin  
+    void'(object_1.randomize());  
+    void'(object_2.randomize());  
+    #1 $display("\t[%0t] @ iteration: %0d -----> value in object_1: %0d  value in object_2: %0d",  
+                $time, i, object_1.value, object_2.value);  
+  end  
+
+  object_3.cons.constraint_mode(0);  
+
+  for (int i = 1; i <= 3; i++) begin  
+    void'(object_3.randomize());  
+    void'(object_4.randomize());  
+    #1 $display("\t[%0t] @ iteration: %0d -----> value in object_3: %0d  value in object_4: %0d",  
+                $time, i, object_3.value, object_4.value);  
+  end  
+end
+
+    //Class 1 has two objects 1&2 and class2 has objects 3&4.In class1 used non-static constraint, turned off in
+    //object 1 but still, the constraint is on in object 2, but used static constraint in class2, and turned off 
+    //in object 3 then the constraint has been turned off in object 4 as well.
